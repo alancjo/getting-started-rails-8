2ccb80b515a7d731e92e7598534a8fe311d8c565
Alan Oliveira <alancjobm@gmail.com>
2025-05-08 12:58:41 -0300

    16.2. Adding Subscribers to Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get "subscribers/create"
   root to: "products#index"
 
   resource :session
   resources :passwords, param: :token
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
 end
